7b4924efb6cd72e249339936bb09ff466f67dcd6
kmkm0310 <79896924+kmkm0310@users.noreply.github.com>
2024-10-06 13:41:53 +0900

    finish_ch05

---
+++ kyouka/ch05/importance_sampling.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+x = np.array([1, 2, 3])
+pi = np.array([0.1, 0.1, 0.8])
+
+# 期待値
+e = np.sum(x * pi)
+print('E_pi[x]', e)
+
+# モンテカルロ法
+n = 100
+samples = []
+for _ in range(n):
+    s = np.random.choice(x, p=pi)
+    samples.append(s)
+
+mean = np.mean(samples)
+var = np.var(samples)
+print(f'MC: {mean:.2f} (var: {var:.2f})')
+
+
+b = np.array([1/3, 1/3, 1/3])
+n = 100
+samples = []
+
+for _ in range(n):
+    idx = np.arange(len(b))
+    i = np.random.choice(idx, p=b)
+    s = x[i]
+    rho = pi[i] / b[i]
+    samples.append(rho * s)
+
+mean = np.mean(samples)
+var = np.var(samples)
+print(f'IS: {mean:.2f} (var: {var:.2f})')
--- kyouka/ch05/mc_control.py
+++ kyouka/ch05/mc_control.py
@@ -6,13 +6,25 @@ import numpy as np
 from collections import defaultdict
 from common.gridworld import GridWorld
 
-class RandomAgent:
+#今後他から使うのでクラスの外に出しておく
+def greedy_probs(Q, state, epsilon=0, action_size=4):
+    qs = [Q[(state, action)] for action in range(action_size)]
+    max_action = np.argmax(qs)
+
+    base_prob = epsilon /action_size
+    action_probs = {action: base_prob for action in range(action_size)}
+    action_probs[max_action] += (1 - epsilon)
+    return action_probs
+
+class McAgent:
     def __init__(self) :
         self.gamma = 0.9
+        self.epsilon = 0.1
+        self.alpha = 0.1
         self.action_size = 4
         random_actions = {0:0.25, 1:0.25, 2:0.25, 3:0.25}
         self.pi = defaultdict(lambda: random_actions)
-        self.V = defaultdict(lambda: 0)
+        self.Q = defaultdict(lambda: 0)
         self.cnts = defaultdict(lambda: 0)
         self.memory = []
         
@@ -23,7 +35,19 @@ class RandomAgent:
         probs = list(action_prob.values())
         return np.random.choice(actions, p=probs)
     
-    
+
+    def update(self):
+        G = 0
+        for data in reversed(self.memory):
+            state, action, reward = data
+            G = self.gamma * G + reward
+            key = (state, action)
+            self.cnts[key] += 1
+            #self.Q[key] += (G - self.Q[key]) / self.cnts[key]
+            self.Q[key] += (G - self.Q[key]) * self.alpha
+            self.pi[state] = greedy_probs(self.Q, state, self.epsilon)
+
+
     def add(self, state, action, reward):
         data = (state, action, reward)
         self.memory.append(data)
@@ -33,34 +57,24 @@ class RandomAgent:
         self.memory.clear()
         
         
-    def eval(self):
-        G = 0
-        for data in reversed(self.memory): # 逆向きにたどる
-            state, action, reward = data
-            G = self.gamma * G + reward
-            self.cnts[state] += 1
-            self.V[state] += (G - self.V[state]) / self.cnts[state]
         
 
 env = GridWorld()
-agent = RandomAgent()
+agent = McAgent()
 episodes = 1000
-cnts = []
+
 for episode in range(episodes):
     state = env.reset()
     agent.reset()
-    cnt = 0
+
     while True:
-        cnt += 1
         action = agent.get_action(state)
         next_state, reward, done = env.step(action)
         
         agent.add(state, action, reward)
         if done:
-            agent.eval()
-            cnts.append(cnt)
+            agent.update()
             break
         state = next_state
         
-print(sum(cnts)/len(cnts))
-env.render_v(agent.V)
+env.render_q(agent.Q)
